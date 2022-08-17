01315d00354ff57075d471b70eaf8a858e5baf51
Cagdas ISIK <cyasam@gmail.com>
2022-08-17 13:13:04 +0000

    Url rewrite added

--- next.config.js
+++ next.config.js
@@ -2,6 +2,26 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en-US', 'tr-TR'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'en-US',    
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/about',
+        destination: '/en/about',
+      },
+      {
+        source: '/hakkimda',
+        destination: '/tr/about',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
---
+++ pages/tr/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function about() {
+  return (
+    <div>hakkımda</div>
+  )
+}
+
+export default about
