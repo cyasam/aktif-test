eb5461d4f72c6b5c818c26bf944203b0a4e6008a
Cagdas ISIK <cyasam@gmail.com>
2022-08-17 15:50:22 +0000

    test

--- next.config.js
+++ next.config.js
@@ -2,23 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  i18n: {
+  /* i18n: {
     // These are all the locales you want to support in
     // your application
     locales: ['en', 'tr'],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: 'en',
-    localeDetection: false,
-  },
+  }, */
   async rewrites() {
     return [
       {
         source: '/hakkimda',
-        destination: '/tr/about'
+        destination: '/tr/about',
+        locale:false
       },
     ]
-  },
+  }, 
 }
 
 module.exports = nextConfig
